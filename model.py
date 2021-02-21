5601cd423b9a4490f1b5b5127ac22ee5df12f662
willyspinner <wilsonjusuf1998@gmail.com>
2021-02-20 16:16:22 -0800

    use base roberta model instead of roberta sequence classification

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from transformers import RobertaTokenizer, RobertaModel
 import torch.nn as nn
 
 model_tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
@@ -8,7 +8,10 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.device = device
         self.tokenizer = model_tokenizer
-        self.roberta_base = RobertaForSequenceClassification.from_pretrained('roberta-base').to(device)
+        self.roberta_base = RobertaModel.from_pretrained('roberta-base').to(device)
+        self.fc1 = nn.Linear(in_features=768, out_features=768, bias=True).to(device)
+        self.dp1 = nn.Dropout(p=0.15, inplace=False).to(device)
+        self.fc2 = nn.Linear(in_features=768, out_features=2, bias=True).to(device)
         self.softmax = nn.Softmax(dim=-1).to(device)
 
     def load(self, model_path):
@@ -25,7 +28,19 @@ class Model(nn.Module):
 
 
     def forward(self, x, **kwargs):
-        # returns a SequenceClassifierOutput, so get logits.
-        logits = self.roberta_base.forward(x, **kwargs).logits.to(self.device)
+
+        # NOTE: RobertaClassificationHead uses the first <s> (start string) token features
+        # to feed into the model.
+        output = self.roberta_base.forward(x, **kwargs).last_hidden_state
+        # output is (batch_Size, ts, 178) where ts = 128
+        # so we take the 0th timestep features and put it to a series of fcs.
+        # this should be ok, since bert is bidirectional.
+        output = output[:, 0, :]
+        # TODO: is 128 a good padding?
+        x = self.fc1(output).to(self.device)
+        x = self.dp1(x).to(self.device)
+        x = self.fc2(x).to(self.device)
+        x = self.softmax(x).to(self.device)
+
         #TODO: perhaps add some layers here? Customize the layers?
-        return self.softmax(logits)
+        return x
